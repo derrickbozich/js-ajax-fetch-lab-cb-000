8ee63e4b70c17bd227fe3eba8ff9792873f49ea6
derrick b <dbozich1@gmail.com>
2018-10-13 18:27:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
-  // const repo = `${user}/js-ajax-fetch-lab-cb-000`;
-  const repo = `learn-co-curriculum/js-ajax-fetch-lab`;
+  const repo = `${user}/js-ajax-fetch-lab-cb-000`;
   const url = `${baseURL}/repos/${repo}/issues`;
   const postData = {
     title: document.getElementById('title').value,
